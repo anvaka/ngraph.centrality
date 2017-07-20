53d03ace11e571366cba3490f8c6b5296b372db2
AVermeij <andrevermeij@gmail.com>
2017-07-20 10:54:34 +0200

    Adding closeness centrality

---
+++ src/closeness.js
@@ -0,0 +1,89 @@
+module.exports = closeness;
+
+function closeness(graph, oriented) {
+  var Q = [],
+    S = []; // Queue and Stack
+  // list of predcessors on shorteest paths from source
+  var pred = Object.create(null);
+  // distance from source
+  var dist = Object.create(null);
+  // number of shortest paths from source to key
+  var sigma = Object.create(null);
+
+  var currentNode;
+  var centrality = Object.create(null);
+  var length = graph.getNodesCount();
+
+  graph.forEachNode(setCentralityToZero);
+  graph.forEachNode(calculateCentrality);
+
+  return centrality;
+
+  function setCentralityToZero(node) {
+    centrality[node.id] = 0;
+  }
+
+  function calculateCentrality(node) {
+    currentNode = node.id;
+    singleSourceShortestPath(currentNode);
+    accumulate();
+  }
+
+  function accumulate() {
+    // Add all distances for node to array, excluding -1s
+    var distances = Object.keys(dist).map(function(key) {return dist[key]}).filter(function(val){return val !== -1});
+    // Set number of reachable nodes
+    var reachableNodesTotal = distances.length;
+    // Compute sum of all distances for node
+    var totalDistance = distances.reduce(function(a,b) { return a + b });
+    if (totalDistance > 0) {
+      centrality[currentNode] = ((reachableNodesTotal - 1) / totalDistance); 
+    } else {
+      centrality[currentNode] = 0;
+    }
+  }
+
+  function singleSourceShortestPath(source) {
+    graph.forEachNode(initNode);
+    dist[source] = 0;
+    sigma[source] = 1;
+    Q.push(source);
+
+    while (Q.length) {
+      var v = Q.shift();
+      var dedup = Object.create(null);
+      S.push(v);
+      graph.forEachLinkedNode(v, toId, oriented);
+    }
+
+    function toId(otherNode) {
+      // NOTE: This code will also consider multi-edges, which are often
+      // ignored by popular software (Gephi/NetworkX). Depending on your use
+      // case this may not be desired and deduping needs to be performed. To
+      // save memory I'm not deduping here...
+      processNode(otherNode.id);
+    }
+
+    function initNode(node) {
+      var nodeId = node.id;
+      pred[nodeId] = []; // empty list
+      dist[nodeId] = -1;
+      sigma[nodeId] = 0;
+    }
+
+    function processNode(w) {
+      // path discovery
+      if (dist[w] === -1) {
+        // Node w is found for the first time
+        dist[w] = dist[v] + 1;
+        Q.push(w);
+      }
+      // path counting
+      if (dist[w] === dist[v] + 1) {
+        // edge (v, w) on a shortest path
+        sigma[w] += sigma[v];
+        pred[w].push(v);
+      }
+    }
+  }
+}
