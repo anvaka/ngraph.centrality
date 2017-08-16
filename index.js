2a4abd7b8399ac435d75e5b1a1dc94a335600c79
AVermeij <andrevermeij@gmail.com>
2017-08-16 09:26:56 +0200

    Added eccentricity to index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.degree = require('./src/degree.js');
 module.exports.betweenness = require('./src/betweenness.js');
 module.exports.closeness = require('./src/closeness.js');
+module.exports.eccentricity = require('./src/eccentricity.js');
